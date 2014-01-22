8f8b9e5615d264606ac5db04e11cdf8bd885c7db
nagoodman <ngoodman@dynamobi.com>
2014-01-21 18:42:29 -0800

    updated some demo files

--- demo/demo.sql
+++ demo/demo.sql
@@ -46,20 +46,30 @@ create or replace foreign table splunk_demo.queue(
 server splunk_server
 options (
 base_search   'search index=_internal source=*metrics.log group="queue"',
-earliest '-24h',
+earliest '-1000d',
 latest 'now'
 );
 
 
 create or replace foreign table splunk_demo.access_logs(
 "_time" double,
+"date_hour" integer,
+"date_mday" integer,
+"date_minute" integer,
+"date_month" varchar(256),
+"date_wday" varchar(256),
+"date_year" integer,
 "host" varchar(256),
 "method" varchar(100),
 "source" varchar(256),
-"category" varchar(256),
 "action" varchar(256),
 "clientip" varchar(256),
-"status" int
+"status" int,
+"file" varchar(256),
+"itemid" varchar(256),
+"uri_query" varchar(256),
+"bytes" float,
+"category_id" varchar(256)
 )
 server splunk_server
 options (
@@ -67,3 +77,32 @@ base_search   'search source=*combined.log',
 earliest '-1000d',
 latest 'now'
 );
+
+create or replace foreign table splunk_demo.access_logs_pentaho(
+"_time" double,
+"date_hour" varchar(256),
+"date_mday" varchar(256),
+"date_minute" varchar(256),
+"date_month" varchar(256),
+"date_wday" varchar(256),
+"date_year" varchar(256),
+"host" varchar(256),
+"method" varchar(100),
+"source" varchar(256),
+"category" varchar(256),
+"action" varchar(256),
+"clientip" varchar(256),
+"status" varchar(256)
+)
+server splunk_server
+options (
+base_search   'search source=*combined.log',
+earliest '-1000d',
+latest 'now'
+);
+
+
+-- For pentaho  OLD
+select "date_hour" as "Date Hour", "date_mday" as "Minute in Day", "date_minute" as "Minute in Hour", "date_month" as "Month of Year", "date_wday" as "Day of Week", "date_year" as "Fiscal Year", "host" as "Host name", "method" as "HTTP Method", "source" as "HTTP Source", "category" as "Event Category", "action" as "Customer Action", "clientip" as "IP Address", "status" as "HTTP Status", "_time" as "Unix Time" from splunk_demo.access_logs;
+
+create or replace view splunk_demo.access_logs_metadata as select t.*, t."method" as "Http Method", t."action" as "Customer Action" from splunk_demo.access_logs t;
